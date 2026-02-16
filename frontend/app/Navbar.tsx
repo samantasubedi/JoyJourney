6b0cab4dd99e87d32efbc10a69ad025a744f373c
samantasubedi <samantasubedi01@gmail.com>
2026-02-16 23:03:38 +0545

    working with navbar

--- frontend/app/Navbar.tsx
+++ frontend/app/Navbar.tsx
@@ -1,3 +1,4 @@
+"use client";
 import React from "react";
 import Link from "next/link";
 import {
@@ -9,7 +10,9 @@ import {
   NavigationMenuTrigger,
 } from "@/components/ui/navigation-menu";
 import Image from "next/image";
+import { usePathname } from "next/navigation";
 const Navbar = () => {
+  const currentRoute = usePathname();
   const destinations = [
     {
       name: "Everest Base Camp",
@@ -40,45 +43,48 @@ const Navbar = () => {
     {
       name: "Adventure",
       desc: "Rafting, paragliding, and intense trails.",
-
       imageName: "/adventure.jpg",
     },
     {
       name: "Luxury",
       desc: "Five-star stays and private helicopters.",
-
       imageName: "/luxury.jpg",
     },
     {
       name: "Budget",
       desc: "Authentic experiences for smart travelers.",
-
       imageName: "/budget.jpg",
     },
     {
       name: "Family",
       desc: "Kid-friendly tours and safe adventures.",
-
       imageName: "/family.jpg",
     },
   ];
+  const navbarClassName =
+    "font-bold group inline-flex h-10 w-max items-center justify-center rounded-full px-4 py-2 text-sm  transition-all hover:bg-slate-100 hover:text-blue-600 focus:bg-slate-100";
   return (
-    <div className="flex justify-center">
-      <NavigationMenu className="z-10 flex max-w-max flex-1 items-center justify-center">
-        <NavigationMenuList className="group flex flex-1 list-none items-center justify-center space-x-1 p-1 bg-white/50 backdrop-blur-md rounded-full border border-slate-200 shadow-sm">
+    <div className="flex justify-center mt-4">
+      <NavigationMenu className="z-10  flex max-w-max flex-1 items-center justify-center">
+        <NavigationMenuList className="group bg-linear-to-b from-purple-50 to-blue-50 flex gap-7 px-3 py-1 flex-1 list-none items-center justify-center space-x-1  backdrop-blur-md rounded-full border border-slate-200 shadow-sm">
           <NavigationMenuItem>
             <NavigationMenuLink
               href="/"
-              className="group inline-flex h-10 w-max items-center justify-center rounded-full px-4 py-2 text-sm font-medium transition-all hover:bg-slate-100 hover:text-blue-600 focus:bg-slate-100 focus:outline-none"
+              className={`${currentRoute === "/" ? "bg-blue-200" : ""} ${navbarClassName}`}
             >
               Home
             </NavigationMenuLink>
           </NavigationMenuItem>
 
           <NavigationMenuItem>
-            <NavigationMenuTrigger className="group inline-flex h-10 w-max items-center justify-center rounded-full px-4 py-2 text-sm font-medium transition-all hover:bg-slate-100 data-[state=open]:bg-slate-100">
-              Destinations
-            </NavigationMenuTrigger>
+            <NavigationMenuLink
+              href="/destinations"
+              className={`${currentRoute === "/destinations" ? "bg-blue-200 " : ""} ${navbarClassName}`}
+            >
+              {" "}
+              <NavigationMenuTrigger>Destinations</NavigationMenuTrigger>
+            </NavigationMenuLink>
+
             <NavigationMenuContent className="border-none absolute top-0 left-0 w-full md:w-[500px] lg:w-[600px] rounded-2xl  bg-white p-6 shadow-2xl animate-in fade-in slide-in-from-top-2 duration-300">
               <div className="mb-4 flex items-center justify-between border-b pb-3">
                 <h4 className="text-sm font-bold uppercase tracking-wider text-slate-500">
@@ -118,16 +124,17 @@ const Navbar = () => {
           </NavigationMenuItem>
 
           <NavigationMenuItem>
-            <NavigationMenuTrigger className="group inline-flex h-10 w-max items-center justify-center rounded-full px-4 py-2 text-sm font-medium transition-all hover:bg-slate-100 data-[state=open]:bg-slate-100">
-              Packages
-            </NavigationMenuTrigger>
+            <NavigationMenuLink
+              href="/packages"
+              className={`${currentRoute === "/packages" ? "bg-blue-200 " : ""} ${navbarClassName}`}
+            >
+              <NavigationMenuTrigger>Packages</NavigationMenuTrigger>
+            </NavigationMenuLink>
             <NavigationMenuContent className="border-none absolute top-0 left-0 w-full md:w-[500px] lg:w-[600px] rounded-2xl  bg-white p-6 shadow-2xl animate-in fade-in slide-in-from-top-2 duration-300">
               <ul className="grid grid-cols-2 gap-2">
                 {packages.map((pkg) => (
                   <li key={pkg.name}>
-                    <NavigationMenuLink
-                    className="hover:bg-slate-200 duration-500 ease-in-out block select-none space-y-1 rounded-xl p-3 leading-none no-underline outline-none transition-all "
-                    >
+                    <NavigationMenuLink className="hover:bg-slate-200 duration-500 ease-in-out block select-none space-y-1 rounded-xl p-3 leading-none no-underline outline-none transition-all ">
                       <Image
                         height={1000}
                         width={2000}
@@ -136,7 +143,9 @@ const Navbar = () => {
                         className="h-30 w-full"
                       ></Image>
 
-                      <div className={`text-sm font-bold transition-colors text-center`}>
+                      <div
+                        className={`text-sm font-bold transition-colors text-center`}
+                      >
                         {pkg.name}
                       </div>
                       <div className="text-xs text-slate-500 leading-relaxed text-center">
@@ -152,7 +161,7 @@ const Navbar = () => {
           <NavigationMenuItem>
             <NavigationMenuLink
               href="/services"
-              className="group inline-flex h-10 w-max items-center justify-center rounded-full px-4 py-2 text-sm font-medium transition-all hover:bg-slate-100 hover:text-blue-600"
+              className={`${currentRoute === "/services" ? "bg-blue-200 " : ""} ${navbarClassName}`}
             >
               Services
             </NavigationMenuLink>
@@ -160,7 +169,7 @@ const Navbar = () => {
           <NavigationMenuItem>
             <NavigationMenuLink
               href="/about"
-              className="group inline-flex h-10 w-max items-center justify-center rounded-full px-4 py-2 text-sm font-medium transition-all hover:bg-slate-100 hover:text-blue-600"
+              className={`${currentRoute === "/about" ? "bg-blue-200 " : ""} ${navbarClassName}`}
             >
               About
             </NavigationMenuLink>
